88afc4ebe3ad4956296bef9d3a4b59f08507eb36
lx <1345940061@qq.com>
2020-03-14 01:22:55 +0800

    [3.14提交]

---
+++ src/views/category/Category.vue
@@ -0,0 +1,117 @@
+<template>
+    <div class="wrapper" ref="aaa">
+        <ul class="content">
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+             <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+             <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+             <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+             <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+             <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+             <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+             <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+             <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+            <li>9999</li>
+
+            
+        </ul>
+    </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+
+export default {
+    name: "Category",
+    data() {
+        return {
+            scroll: null
+        }
+    },
+    mounted() {
+        this.scroll = new BScroll(document.querySelector('.wrapper'), {
+            
+        })
+    },
+}
+</script>
+
+<style scoped>
+    .wrapper {
+        height: 150px;
+        background-color: red;
+
+        /* overflow: hidden; */
+        /* overflow: scroll; */
+    }
+</style>
